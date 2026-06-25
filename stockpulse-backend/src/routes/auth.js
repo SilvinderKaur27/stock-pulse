f09ec7cebd0b607c2c2c638120f27a7b806d09f9
SilvinderKaur27 <kaursilvi27@gmail.com>
2026-06-25 09:23:51 +0530

    step 7: refresh token and logout working — phase 1 complete

--- stockpulse-backend/src/routes/auth.js
+++ stockpulse-backend/src/routes/auth.js
@@ -61,20 +61,109 @@ router.post('/login', async (req, res)=> {
         })
     }
 
-    const token = jwt.sign(
+    const accessToken = jwt.sign(
         { id: user.id }, 
         process.env.JWT_SECRET,
-        { expiresIn: "1h"}
+        { expiresIn: "15m"}
+    )
+    const refresh_token = jwt.sign({
+        id: user.id},
+        process.env.JWT_SECRET,
+        {expiresIn: "7d"}
     )
 
+    await pool.query(
+        `UPDATE users 
+        SET refresh_token = $1
+        WHERE id = $2`,[refresh_token, user.id])
+
     res.json({ 
-        token
+        accessToken,
+        refresh_token: refresh_token
 })
     } catch(error) {
         res.status(500).json({
             message: error.message
         })
     }
+})
+
+router.post("/refresh", async (req, res) => {
+    try {
+        const { refreshToken } = req.body;
+
+        if(!refreshToken) {
+            return res.status(401).json({
+                message: "Refresh token required"
+            });
+        }
+
+        const decoded = jwt.verify(
+            refreshToken,
+            process.env.JWT_SECRET
+        )
+
+        const result = await  pool.query(
+            "SELECT * FROM users WHERE id = $1",
+            [decoded.id]
+        )
+
+        const user = result.rows[0];
+
+        if (!user || user.refresh_token !== refreshToken) {
+            return res.status(401).json({
+                message: "Invalid refresh token"
+            })
+        }
+    const accessToken = jwt.sign(
+            { id: user.id },
+            process.env.JWT_SECRET,
+            { expiresIn: "15m" }
+        );
+
+        res.json({
+            accessToken
+        });
+
+    } catch(error) {
+        res.status(401).json({
+            message: "Invalid refresh token"
+        });
+    }
+    })
+
+router.post("/logout", async(req, res)=> {
+    try{
+        const { refreshToken } = req.body;
+
+        if (!refreshToken) {
+            return res.status(400).json({
+                message: "Refresh token required"
+            });
+        }
+
+        const decoded = jwt.verify(
+            refreshToken,
+            process.env.JWT_SECRET
+        );
+
+        await pool.query(
+            `UPDATE users
+             SET refresh_token = NULL
+             WHERE id = $1`,
+            [decoded.id]
+        );
+
+        res.json({
+            message: "Logged out successfully"
+        });
+
+    } catch (error) {
+        res.status(401).json({
+            message: "Invalid refresh token"
+        });
+    }
+
 })
 
 module.exports = router;
